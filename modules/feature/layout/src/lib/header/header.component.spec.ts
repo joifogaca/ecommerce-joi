fa9e16da7d682263bd4b5614b2a91474be26e54a
Joice Fogaça <joi.fogaca@gmail.com>
2024-07-04 16:51:27 -0300

    correcao lint

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
+const mockTitle = 'mock';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -12,6 +13,7 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.title = mockTitle;
     fixture.detectChanges();
   });
 
@@ -20,8 +22,12 @@ describe('HeaderComponent', () => {
   });
 
   it('should contain title', () => {
-    const header: HTMLHeadingElement = 
-    fixture.nativeElement.querySelector('header');
-    expect(header.textContent).toBe('Ecommerce');
+    const header: HTMLHeadingElement =
+      fixture.nativeElement.querySelector('header');
+    expect(header.textContent).toBe(mockTitle);
+
+    component.title = 'other';
+    fixture.detectChanges();
+    expect(header.textContent).toBe('other');
   });
 });
