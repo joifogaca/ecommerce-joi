5775e3e4e950ac3346372a4334b3cdfcd30f2fc8
Joice Fogaça <joi.fogaca@gmail.com>
2024-07-05 15:01:24 -0300

    feat(product-data-access) add product search service

---
+++ modules/data-access/product/src/lib/mocks/product.mock.ts
@@ -0,0 +1,34 @@
+import { Product } from '../models/product.model';
+
+export const mockProducts: Product[] = [
+  {
+    createdAt: '2023-12-12T01:29:19.429Z',
+    name: 'Intelligent Wooden Mouse',
+    price: '113.00',
+    description:
+      'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016',
+    image: 'https://loremflickr.com/640/480/transport',
+    quantity: 1,
+    id: '1',
+  },
+  {
+    createdAt: '2023-12-12T04:07:04.564Z',
+    name: 'Handmade Soft Towels',
+    price: '390.00',
+    description:
+      'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+    image: 'https://loremflickr.com/640/480/city',
+    quantity: 0,
+    id: '2',
+  },
+  {
+    createdAt: '2023-12-12T02:34:18.491Z',
+    name: 'Unbranded Granite Pants',
+    price: '739.00',
+    description:
+      'The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive',
+    image: 'https://loremflickr.com/640/480/cats',
+    quantity: 22,
+    id: '3',
+  },
+];
